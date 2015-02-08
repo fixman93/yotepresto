dcc7cb7e6133449fe2a88d462da54e9af46b9f14
Boris Civcic <civcicboris93@gmail.com>
2015-02-08 20:20:31 +0100

    update lista-do page

---
+++ lista-do.php
@@ -0,0 +1,183 @@
+<?php include 'header.php'; ?>
+<div class="investor-undermenu">
+			<div class="container-fluid">
+				<div class="col-sm-6 investor-h3">
+					<h3>Tus solicitudes de crédito</h3>
+				</div>
+				<div class="col-sm-6">
+					<h3>Saldo de capital</h3>
+					<h4>$0000.00</h4>
+				</div>
+			</div> <!-- /.container-fluid -->
+		</div> <!-- /.investor-undermenu -->
+<div class="lend-list-cover">
+	<div class="table-responsive container-fluid">
+		<table class="table lend-list">
+			<thead>
+				<tr>
+				    <td>Calificación</td>
+					<td>Destino</td>
+					<td>Titulo del Crédito</td>
+					<td>Monto/Tasa/Plazo</td>
+					<td>Prestado Tiempo de vigencia</td>
+				</tr>
+			</thead>
+			 <tbody>
+				<tr>
+					<td><h1>A1</h1></td>
+					<td><img src="img/list-pic1.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$97,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>B2</h1></td>
+					<td><img src="img/list-pic2.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$130,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>A2</h1></td>
+					<td><img src="img/list-pic3.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$97,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>A3</h1></td>
+					<td><img src="img/list-pic4.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$800,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>C2</h1></td>
+					<td><img src="img/list-pic5.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$1'0097,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>B7</h1></td>
+					<td><img src="img/list-pic6.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$130,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>A4</h1></td>
+					<td><img src="img/list-pic7.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$97,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+				<!-- lend-list table row -->
+				<tr>
+					<td><h1>C3</h1></td>
+					<td><img src="img/list-pic8.png"></td>
+					<td>No choque,<br>me chocaron!!</td>
+					<td>
+						<h2>$130,0000</h2>
+						<div class="monto-price clearfix">
+							<span>%0.12</span><span>6años</span>
+						</div>
+					</td>
+					<td>
+						<div class="prestado-tiempo clearfix"></div>
+						<div class="pull-left">80%</div>
+						<div class="pull-right"><span>10d</span><span>6h</span><span>24min</span></div>
+						
+					</td>
+				</tr>
+				<!-- end of lend list table row -->
+			</tbody>
+		</table>
+	</div>
+</div>
+
+
+
+<?php include 'footer.php'; ?>
