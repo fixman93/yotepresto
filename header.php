0acb75a8d5f4416e92662c4f85a5d33ba12673dc
Marko Bozic <marko_bozic@yahoo.com>
2015-02-08 01:44:53 +0100

    Investor-preference
    Napravljena nova strana

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <link href="css/bootstrap-select.css" rel="stylesheet">
     <link href="css/font-awesome.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+    <link href="css/marko.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <![endif]-->
---
+++ investor-preference.php
@@ -0,0 +1,58 @@
+	<?php include 'header.php'; ?>
+		<div class="investor-undermenu">
+			<div class="container-fluid">
+				<div class="col-sm-6 investor-h3">
+					<h3>Tus solicitudes de crédito</h3>
+				</div>
+				<div class="col-sm-6">
+					<h3>Saldo de capital</h3>
+					<h4>$0000.00</h4>
+				</div>
+			</div> <!-- /.container-fluid -->
+		</div> <!-- /.investor-undermenu -->
+		<div class="invenstor-content">
+			<div class="container-fluid">
+				<h3>Mi perfil</h3>
+				<div class="investor-form">
+					<div class="col-sm-12">
+						<div class="investor-form-content clearfix">
+							<div class="investor-options">
+								<img src="img/user.png" alt="">
+								<h4>Información<br>Personal</h4>
+							</div>
+							<div class="investor-options">
+								<img src="img/money-book.png" alt="">
+								<h4>Cuentas<br>de Fondeo</h4>
+							</div>
+							<div class="investor-options">
+								<img src="img/money.png" alt="">
+								<h4>Cuentas<br>de Retiro</h4>
+							</div>
+							<div class="investor-options">
+								<img src="img/checked.png" alt="">
+								<h4>Preferencias<br>de inversión</h4>
+							</div>
+							<div class="investor-options">
+								<img src="img/lock.png" alt="">
+								<h4>Cambiar<br>Contraseña</h4>
+							</div>
+						</div>
+					</div> <!-- /.col-sm-12 -->
+					<form class="investor-form-form">
+						<input type="text">
+						<label>Contraseña actual</label>
+						<input type="text">
+						<label>Contraseña nueva</label>
+						<input type="text">
+						<label>Verificar contraseña nueva</label>
+						<div class="investor-form-button">
+							<button type="button" class="btn btn-primary btn-sm">Actualizar</button>
+						</div> <!-- /.investor-form-button -->
+					</form> <!-- /.investor-form -->
+				</div> <!-- /.investor-options -->
+			</div> <!-- /.container-fluid -->
+		</div> <!-- /.invenstor-content -->
+	<?php include 'footer.php'; ?>
+	
+		
+	
