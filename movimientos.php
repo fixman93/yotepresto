667828286a0b3482dd1b185f146f30e01d29c9b9
Boris Civcic <civcicboris93@gmail.com>
2015-02-08 15:26:26 +0100

    update new page movements

---
+++ movimientos.php
@@ -0,0 +1,56 @@
+<?php include 'header.php'; ?>
+<div class="creditos-fondear">
+	<div class="container-fluid">
+		<div class="col-sm-4">
+			<h1>Créditos a Fondear: <span>3</span></h1>
+		</div>
+		<div class="col-sm-6">
+			<h1>Monto total a prestar: <span>$ 15,200.00</span></h1>
+		</div>
+		<div class="col-sm-2 fondear-btn">
+			<button>Prestar</button>
+			<button>Var detaille</button>
+		</div>
+	</div>
+</div>
+<h1 class="agregar">Completa los datos de tu perfil</h1>
+<div class="completa-los-datos">
+	<div class="container-fluid clearfix">
+		<div class="col-sm-6">
+			<input type="text" class="form-control">
+			<label>Primer nombre</label>
+			<input type="text" class="form-control">
+			<label>Primer apellido</label>
+			<input type="text" class="form-control">
+			<label>RFC</label>
+			<input type="text" class="form-control">
+			<label>CURP</label>
+		</div>
+		<div class="col-sm-6">
+			<input type="text" class="form-control">
+			<label>Segundo nombre</label>
+			<input type="text" class="form-control">
+			<label>Segundo apellido</label>
+			<input type="text" class="form-control">
+			<label>Fecha de nacimiento</label>
+			<input type="text" class="form-control">
+			<label>¿Como te enteraste de nosotros?</label>
+		</div>
+	</div>
+</div>
+<div class="completa-social clearfix">
+	<div class="container-fluid">
+		<div class="col-sm-6 face-login">
+			<a href="#"><img src="img/facebook-login.png"></a>
+		</div>
+		<div class="col-sm-6 completa-btn">
+			<button>Guardar y Continuar</button>
+			<button>Cerrar</button>
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php include 'footer.php'; ?>
