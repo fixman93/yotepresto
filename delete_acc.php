3535cee49169f767aebc6359cb179c823f90d0aa
Boris Civcic <civcicboris93@gmail.com>
2015-02-10 00:45:01 +0100

    change pass

---
+++ delete_acc.php
@@ -0,0 +1,102 @@
+<?php include 'header.php'; ?>
+<div class="creditos-fondear">
+	<div class="container-fluid">
+		<div class="col-sm-4">
+			<h1>Créditos a Fondear: <span>3</span></h1>
+		</div>
+		<div class="col-sm-6">
+			<h1>Monto total a prestar: <span>$ 15,200.00</span></h1>
+		</div>
+		<div class="col-sm-2 fondear-btn">
+			<button>Prestar</button>
+			<button>Var detaille</button>
+		</div>
+	</div>
+</div>
+<div class="pass-for">
+	<div class="container-fluid">
+		<h1>MI PERFIL</h1>
+		<div class="informacion-personal clearfix">
+			<div class="personal-info">
+				<div><i class="fa fa-user"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-credit-card"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-credit-card"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-users"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-lock"></i></div>
+				Informacion Personal
+			</div>
+		</div>
+		<div class="table-responsive numero-banc">
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<td>Description</td>
+					<td>Numero CLABE</td>
+					<td>Banco</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1sda25</td>
+					<td>232desa00</td>
+					<td>3dsad3225</td>
+				</tr>
+				<tr>
+					<td>1dsa3225</td>
+					<td>2032430</td>
+					<td>325</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+		<div class="actualizar"><button>Actualizar</button></div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
