6b35e510d6fc410848312b72dca3eecfd51e1465
Boris Civcic <civcicboris93@gmail.com>
2015-02-10 01:00:51 +0100

    new files

---
+++ change_pass.php
@@ -0,0 +1,49 @@
+<?php include 'header.php'; ?>
+<div class="creditos-fondear">
+	<div class="container-fluid">
+		<div class="col-sm-4">
+			<h1>Créditos a Fondear: <span>3</span></h1>
+		</div>
+		<div class="col-sm-6">
+			<h1>Monto total a prestar: <span>$ 15,200.00</span></h1>
+		</div>
+		<div class="col-sm-2 fondear-btn">
+			<button>Prestar</button>
+			<button>Var detaille</button>
+		</div>
+	</div>
+</div>
+<div class="pass-for">
+	<div class="container-fluid">
+		<h1>MI PERFIL</h1>
+		<div class="informacion-personal clearfix">
+			<div class="personal-info">
+				<div><i class="fa fa-user"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-credit-card"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-credit-card"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-users"></i></div>
+				Informacion Personal
+			</div>
+			<div class="personal-info">
+				<div><i class="fa fa-lock"></i></div>
+				Informacion Personal
+			</div>
+		</div>
+		<div class="form-group">
+			<input class="form-control" type="text">
+			<input class="form-control" type="text">
+			<input class="form-control" type="text">
+		</div>
+		<div class="actualizar"><button>Actualizar</button></div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
